d425b19509b7f7688bb2d0204e16fd7a33a741f7
itmuvunyi <i.muvunyi@alustudent.com>
2026-07-05 20:05:29 +0200

    Added the chosen keywords, calculated the prior, and started the loop for the bayes theorem, scanning through the dataset

--- part2_bayesian_probability/bayesian_probability.py
+++ part2_bayesian_probability/bayesian_probability.py
@@ -19,3 +19,22 @@ with open("IMDB Dataset.csv", mode="r", encoding="utf-8") as file:
         if sentiment == "positive":
             positive_reviews += 1
 
+# We define keywords
+keywords = ["excellent", "amazing", "great", "terrible", "bad", "boring"]
+
+# Calculate Prior: P(Positive)
+p_positive = positive_reviews / total_reviews
+print(f"Prior P(Positive): {p_positive * 100:.2f}%\n")
+
+# Loop througheach keyword to calculate Bayes Theorem
+for keyword in keywords:
+    keyword_total_count = 0
+    keyword_in_positive_count = 0
+
+    # Scan through every review in the saved dataset
+    for item in dataset:
+        if keyword in item["text"]:
+            keyword_total_count += 1
+            if item["sentiment"] == "positive":
+                keyword_in_positive_count += 1
+
